5bce05bd88c5c36636f5cff631a7801666f91870
srajkumar1997 <91585071+srajkumar1997@users.noreply.github.com>
2021-09-29 15:11:28 +0530

    intrest

---
+++ intrest.py
@@ -0,0 +1,7 @@
+def compound_interest(principle, rate, time):
+ 
+    # Calculates compound interest
+    Amount = principle * (pow((1 + rate / 100), time))
+    CI = Amount - principle
+    print("Compound interest is", CI)
+compound_interest(20000, 10.25, 5)
